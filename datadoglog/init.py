0dcf2db91999b2daae6de991cce1891ee519de82
damon <damon@mahuika.co.nz>
2021-02-05 15:27:48 +1300

    init_logging replace_existing_handlers

--- datadoglog/init.py
+++ datadoglog/init.py
@@ -4,7 +4,11 @@ import sys
 from .formatter import DatadogFormatter
 
 
-def init_logging():
+def init_logging(replace_existing_handlers=False):
     log_handler = logging.StreamHandler(sys.stdout)
     log_handler.setFormatter(DatadogFormatter())
-    logging.getLogger().addHandler(log_handler)
+    root_logger = logging.getLogger()
+    if replace_existing_handlers:
+        for handler in root_logger.handlers:
+            root_logger.removeHandler(handler)
+    root_logger.addHandler(log_handler)
