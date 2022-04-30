15dc9c5ab3a2c11009ea0496c616d2128dbfc83c
damon <damon@mahuika.co.nz>
2022-05-01 11:13:29 +1200

    Remove Datadog unified tagging attributes if blank

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
 # Change log history
 
+## 1.4.0
+
+- If Datadog unified tag attributes are set but blank then remove them so Datadog will fallback to trying to determine
+  them from other sources (for example the container producing the logs)
+
 ## 1.3.0
 
 - Add more standard attributes:
-  - `logger.thread_name`
-  - `logger.method_name`   
+    - `logger.thread_name`
+    - `logger.method_name`   
--- datadoglog/formatter.py
+++ datadoglog/formatter.py
@@ -2,15 +2,17 @@ from pythonjsonlogger import jsonlogger
 
 # From: https://github.com/jd/daiquiri/blob/master/daiquiri/formatter.py
 
+DATADOG_UNIFIED_TAG_ATTRIBUTES = {"dd.env", "dd.service", "dd.version"}
+
 
 class DatadogFormatter(jsonlogger.JsonFormatter):
     def __init__(self):
         super().__init__(timestamp=True)
 
     def add_fields(self, log_record, record, message_dict):
-        super(DatadogFormatter, self).add_fields(log_record, record, message_dict)
+        super().add_fields(log_record, record, message_dict)
         log_record["status"] = record.levelname.lower()
-        log_record.pop("level", None)
+        del log_record["level"]
         logger_dict = {
             "name": record.name,
             "method_name": f"{record.module}.{record.funcName}",
@@ -24,5 +26,10 @@ class DatadogFormatter(jsonlogger.JsonFormatter):
                 "stack": message_dict.get("stack_info"),
                 "message": message_dict.get("exc_info"),
             }
-            log_record.pop("exc_info", None)
-            log_record.pop("stack_info", None)
+            del log_record["exc_info"]
+            del log_record["stack_info"]
+        for attr in DATADOG_UNIFIED_TAG_ATTRIBUTES:
+            # If not set explicitly these will be blank, blocking them being set through other means
+            # For example, determined based on the container producing the logs
+            if attr in log_record and log_record[attr] == "":
+                del log_record[attr]
